a93265607efde7e6d4528d0b6a0185da3c6db71b
Nisha <rainishani400@gmail.com>
2025-04-11 16:08:04 +0545

    Add files via upload

---
+++ Backend2025/script.js
@@ -0,0 +1,12 @@
+//let  n =6;
+//for(let i=0;i<n; i++){
+    //console.log("hello ," ,i);
+//}
+//console.log("bye!");
+//const someValue = require("./math");
+//console.log(someValue);
+const math = require("./math");
+console.log(math.sum(4,4));
+console.log(math.PI);
+console.log(math.g);
+console.log(math.mul(8,8));
