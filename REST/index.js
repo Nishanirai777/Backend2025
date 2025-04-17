4b182e524b1298f6c7c857356d59afbe6bb4ca86
Nisha <rainishani400@gmail.com>
2025-04-17 08:13:42 +0545

    Update index.js

--- REST/index.js
+++ REST/index.js
@@ -1,45 +1,34 @@
 const express = require("express");
 const app = express();
-const port = 4040;
+const port = 3030;
 const path = require("path");
-
+const methodOverride = require("method-override");
 const { v4: uuidv4 } = require('uuid');
-
-
+ // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
-
-
 let posts = [
     {
-        id: uuidv4(), // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-        username: "apnacollege",
+        id: uuidv4(),
+        username: "United College",
         content: "i love coding",
 
+    },
+    {
+        id: uuidv4(),
+        username: "NishaRai",
+        content: "Hard working is important to achieve sucess",
+    },
+    {
+        id: uuidv4(),
+        username: "SarajDhakal",
+        content: "very talented man",
 
-    },
-    {
-        id: uuidv4(), // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
-        username: "Nisha Rai",
-        content: "hard working is important to achieve the sucess",
-    },
-    {
-        id: uuidv4(), // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
-        username: "maite rai",
-        content: "ione day t will show you my daughter ",
     },
 ];
-
-
-
-
-
-
-
-
-
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 });
@@ -47,31 +36,42 @@ app.get("/posts/new", (req, res) => {
     res.render("new.ejs");
 });
 app.post("/posts", (req, res) => {
-    //console.log(req.body);
     let { username, content } = req.body;
     let id =uuidv4();
-    posts.push({ id, username, content });
-    //res.send("post request working");
+    posts.push({  id, username, content });
     res.redirect("/posts");
-});
-app.path("/posts/:id",(req,res)=>{
-    let{id}=req.params;
-    console.log(id);
-    res.send("path request request working");
+    //res.send("post request working")
 });
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
     console.log(id);
     let post = posts.find((p) => id === p.id);
-    res.render("show.ejs", {post});
-   
-});
-app.get("/posts/:id/edit",(req,res)=>{
-    let { id } = req.params;
-    let post = posts.find((p) => id === p.id);
-    res.render("edit.ejs");
+    res.render("show.ejs", { post });
+    // res.send("request working");
 });
 
+app.patch("/posts/:id",(req, res)=>{
+    let {id}= req.params;
+    let newContent =req.body.content;
+    let post =posts.find((p)=> id === p.id)
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+});
+app.get("/posts/:id/edit",(req, res)=>{
+    let {id}= req.params;
+    let post =posts.find((p)=> id === p.id)
+    res.render("edit.ejs");
+});
+//app.get("/", (req, res) => {
+// res.send("serving working well!");
+
+//});
+app.delete("/posts/:id",(req, res)=>{
+    let {id}= req.params;
+     posts = posts.filter((p)=> id  !== p.id)
+     res.redirect("/posts");
+});
 app.listen(port, () => {
-    console.log("listening to port :4040");
+    console.log("listening to port :3030");
 });
