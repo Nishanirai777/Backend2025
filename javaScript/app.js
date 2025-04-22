03331193b233e621acd466ef2e7e5bb9f3b5341b
Nisha <rainishani400@gmail.com>
2025-04-22 20:25:14 +0545

    Update app.js

--- javaScript/app.js
+++ javaScript/app.js
@@ -4,3 +4,11 @@ let pen = 60;
 let copy= 100;
 let output="the total cost are:" + (pen + copy) +"rupees";
 console.log(output);
+
+
+
+//templates
+console.log("hellow worlds")
+let pen = 60;
+let copy= 100;
+console.log(`the total cost are:${pen + copy} Rupees.`);
