18703dc0750bfd4e1a94072e28011848cc6260a3
Nisha <rainishani400@gmail.com>
2025-04-23 14:46:30 +0545

    Add files via upload

---
+++ javaScript/traffic light System/app.js
@@ -0,0 +1,15 @@
+//traffic light System
+let color = "blue";
+if (color === "red") {
+    console.log("Stop! the light color is red now..");
+}
+  else if (color === "yellow") {
+    console.log("slow down the light color is yellow now");
+ }
+ else if (color === "green") {
+    console.log("Go light color is green");
+}else{
+    console.log("traffic light is broken!(sorry)");
+}
+
+
