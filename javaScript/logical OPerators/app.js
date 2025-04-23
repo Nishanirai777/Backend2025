89796fd30fc87529a2707244fe724431b2615dcb
Nisha <rainishani400@gmail.com>
2025-04-23 15:19:45 +0545

    Add files via upload

---
+++ javaScript/logical OPerators/app.js
@@ -0,0 +1,12 @@
+//nested if else
+let marks =9;
+if(marks>=33){
+    console.log("pass");
+    if(marks>=80){
+        console.log("grade:outstanding");
+    }else{
+        console.log("grade:A");
+    }
+}else {
+    console.log("sorry better luck next time");
+}
