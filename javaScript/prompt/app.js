2bbe55095dab7e8daa9480102ec70a96628441a6
Nisha <rainishani400@gmail.com>
2025-04-24 15:13:25 +0545

    Add files via upload

---
+++ javaScript/prompt/app.js
@@ -0,0 +1,7 @@
+//let  firstName=  prompt("enter your name:-");
+//console.log(firstName);
+let firstName =prompt("enter your first name:");
+let lastName= prompt("enter your lastName:");
+//console.log("welcome ..", firstName , " ", lastName);
+let msg= "welcome " + firstName +" " + lastName + "!";
+alert(msg);
