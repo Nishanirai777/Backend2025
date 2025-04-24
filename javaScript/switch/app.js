e9959c96661bab3f1331f7ef4ba0c585ae8ef182
Nisha <rainishani400@gmail.com>
2025-04-24 14:55:07 +0545

    Add files via upload

---
+++ javaScript/switch/app.js
@@ -0,0 +1,45 @@
+//switch
+let color = "red";
+switch(color){
+    case "red":
+        console.log("stop");
+        break;
+        case "yellow":
+            console.log("slow down");
+            break;
+            case "green":
+                console.log("go");
+                break;
+                default:
+                    console.log("light is broken");
+}
+let day =1;
+switch(day){
+    case 1 :
+        console.log("sunday");
+        break;
+        case 2:
+            console.log("monday");
+            break;
+            case 3:
+                console.log("tuesday");
+                break;
+                case 4:
+                    console.log("wednesday");
+                    break;
+                    case 5:
+                        console.log("thursday");
+                        break;
+                        case 6:
+                            console.log("friday");
+                            break;
+                            case 7:
+                                console.log("saturday");
+                                break;
+                                default:
+                                    console.log("wrong value");
+}
+alert("this is a simple alerts...!");
+console.log("this is a simple log");
+console.error("this is an error msg");
+console.warn("this is a warning msg");
