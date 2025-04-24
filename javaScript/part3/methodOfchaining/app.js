f315d68e0c18c59c3c51ae596a8b7c90e957aa14
Nisha <rainishani400@gmail.com>
2025-04-24 15:58:48 +0545

    Add files via upload

---
+++ javaScript/part3/methodOfchaining/app.js
@@ -0,0 +1,8 @@
+let msg = "hello";
+//let newMsg= msg.trim();
+//console.log( " after trim :",newMsg);
+//newMsg=  newMsg.toLocaleUpperCase();
+//console.log("after upperCase:", newMsg);
+//in method chaining
+let  newMsg = msg.trim().toLowerCase();
+console.log(newMsg);
