e7fbad900f5e4914e18e6b92e1ab52baa214b43c
Nisha <rainishani400@gmail.com>
2025-04-27 07:05:27 +0545

    Add files via upload

---
+++ javaScript/part3/ps/app.js
@@ -0,0 +1,9 @@
+let nish= ["january","july","march","august"];
+nish.shift();
+console.log(nish);
+nish.shift();
+console.log(nish);
+nish.unshift("june");
+console.log(nish);
+nish.unshift("july");
+console.log(nish);
